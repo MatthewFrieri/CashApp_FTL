032ba3f59feccfa2bad694b7fc2bf7c3cd2e29a1
avantitandon <157421594+avantitandon@users.noreply.github.com>
2024-09-30 09:02:56 -0400

    Database connection (can change if we do it remotely)

---
+++ backend/database.py
@@ -0,0 +1,17 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            password=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    
+    return connection
