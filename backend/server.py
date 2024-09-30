c5c3deab9f2692d4c0bbf8db716e96297b614c30
Matthew Frieri <mat.frieri@gmail.com>
2024-09-29 21:27:08 -0400

    Working API call to back end.

--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,14 @@
-print('hi')
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask('server')
+CORS(app)
+
+# Example of an endpoint that returns test data
+@app.route('/getinfo')
+def getinfo():
+    info = {"name":'breaking bias', "score":"awesome"}
+    return jsonify(info)
+
+if __name__ == '__main__':
+    app.run()
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const serverURL = "http://127.0.0.1:5000";
+
+  async function makeTestGetRequest() {
+    const endpoint = serverURL + "/getinfo";
+
+    try {
+      // make the request
+      const response = await fetch(endpoint);
+
+      // catch errors in fetch response
+      if (!response.ok) {
+        throw new Error("Error in response:" + response.statusText);
+      }
+
+      // convert the data to json
+      const data = await response.json();
+
+      console.log(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>This is our front end</h1>
+      <button onClick={makeTestGetRequest}>
+        Click me to get some info from the back end
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
